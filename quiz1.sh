bdff3bee52d68d239a5cfc11f01bbfe50b6e2045
Khalidsaif11 <Khalidab33@gmail.com>
2024-01-23 10:49:40 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -1,1 +1,12 @@
-Khalid Saif Khalidsaif11
+Khalid Saif Khalidsaif11			#co authered with Natael
+
+#Task 3
+
+gunzip dictionary.gz | grep a | grep -E "^[mcuotfa]{4,}$"
+gunzip dictionary.gz | grep b | grep -E "^[trnliab]{4,}$"
+gunzip dictionary.gz | grep c | grep -E "^[amdinoac]{4,}$"
+gunzip dictionary.gz | grep z | grep -E "^[naigroz]{4,}$"
+
+#task 4
+
+gunzip -c jaspar2024_core.transfac.gz | grep tax | cut -d ":" -f 2 | sort
