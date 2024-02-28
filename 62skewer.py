88087411425696e30a1aec0ca0afe81afaddef33
Khalidsaif11 <Khalidab33@gmail.com>
2024-02-27 21:36:20 -0800

    new

--- 62skewer.py
+++ 62skewer.py
@@ -9,26 +9,6 @@ w = int(sys.argv[2])
 genome = sys.argv[1]
 
 
-for defline, sequence in mcb185.read_fasta(genome):
-	initial_window = sequence[0:w]
-	G_count = initial_window.count('G')
-	C_count = initial_window.count('C')
-	for i in range(len(sequence) - w):
-		drop = sequence[i]
-		pick = sequence[i+w]
-		if drop == 'G': G_count -= 1
-		elif drop == 'C': C_count -= 1
-		if pick == 'G': G_count += 1
-		elif pick == 'C': C_count += 1
-		
-		gc_comp = (G_count + C_count) / w
-		if G_count + C_count != 0:
-			gc_skew = (G_count - C_count) / (G_count + C_count)
-		else:
-			gc_skew = 0
-		print(f'{i}\t{gc_comp:.3f}\t{gc_skew:.3f}')
-		
-		
 '''
 for size 10
 real    1m36.543s
